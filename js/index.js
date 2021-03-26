104b887cca268494b93d79a544285a284ef9e992
4boysdontcry <hgy02166@gmail.com>
2021-03-26 14:30:39 +0900

    03.26 5r

--- js/index.js
+++ js/index.js
@@ -1,7 +1,21 @@
-$(".bars-wrapper").click(function(){
+$(".bars-wrapper").click(function(e){
     $('.monavi-wrapper').toggleClass('active')
 })
 
+$('.tour-wrapper .bt-buy').click(function(e){
+    $('.modal-wrapper').addClass('active')
+    $('.modal-wrapper > .modal-wrap').css('transform')
+    $('.modal-wrapper > .modal-wrap').css('transform', 'translateY(0)')
+})
+
+$('.modal-wrapper, .modal-wrapper .bt-close').click(function(e){
+    $('.modal-wrapper').removeClass('active')
+    $('.modal-wrapper > .modal-wrap').css('transform', 'translateY(-100vh)')
+})
+
+$('.modal-wrapper, .modal-wrap').click(function(e){
+    e.stopPropagation()
+})
 
 var swiper = new Swiper('.main-wrapper .swiper-container', {
     loop: true,
@@ -12,4 +26,18 @@ var swiper = new Swiper('.main-wrapper .swiper-container', {
         nextEl: '.main-wrapper .bt-next',
         prevEl: '.main-wrapper .bt-prev',
     },
+})
+
+var center = new kakao.maps.LatLng(37.55523393911171, 126.93687362502095)
+var container = document.getElementById('map');
+var options = {
+	center: center,
+	level: 2
+};
+
+var map = new kakao.maps.Map(container, options);
+map.setZoomable(false);
+
+$(window).resize(function(){
+    map.setCenter(center);
 })
